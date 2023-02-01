1f3fb35d4aea8b842a85e1194e8086e1a6b07485
Seher Dogangüzel <seker.seher@hotmail.de>
2023-02-01 12:03:25 +0100

    landing-page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DialogComponent } from './components/dialog/dialog.component';
+import { ImpressumComponent } from './components/impressum/impressum.component';
+import { LoginComponent } from './components/login/login.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'dialog',
+  component: DialogComponent},
+
+  {path:'landingPage',
+  component: LandingPageComponent},
+
+  {path:'login',
+  component: LoginComponent},
+
+  {path:'impressum',
+  component: ImpressumComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
